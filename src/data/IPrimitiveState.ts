0cb57f5ee33e04c1aef40f4fdb27ced14152d3a2
feng <908087098@qq.com>
2024-12-13 01:58:42 +0800

    IPrimitiveState.topology

--- src/data/IPrimitiveState.ts
+++ src/data/IPrimitiveState.ts
@@ -8,12 +8,18 @@
  */
 export interface IPrimitiveState
 {
-    // /**
-    //  * The type of primitive to be constructed from the vertex inputs.
-    //  *
-    //  * WebGPU 默认 `"triangle-list"` ,默认每三个顶点绘制一个三角形。
-    //  */
-    // readonly topology?: GPUPrimitiveTopology;
+    /**
+     * The type of primitive to be constructed from the vertex inputs.
+     *
+     * * `point-list` 绘制单个点。
+     * * `line-strip` 绘制连线
+     * * `line-list` 每两个顶点绘制一条线段。
+     * * `triangle-list` 每三个顶点绘制一个三角形。
+     * * `triangle-strip` 绘制三角形条带。
+     * 
+     * 默认 `triangle-list` ,默认每三个顶点绘制一个三角形。
+     */
+    readonly topology?: IPrimitiveTopology;
 
     // /**
     //  * Defines which polygon orientation will be culled, if any.
@@ -31,4 +37,15 @@ export interface IPrimitiveState
     //  * Requires the {@link GPUFeatureName#"depth-clip-control"} feature to be enabled.
     //  */
     // readonly unclippedDepth?: boolean;
+}
+
+export type IPrimitiveTopology = IPrimitiveTopologyMap[keyof IPrimitiveTopologyMap];
+
+export interface IPrimitiveTopologyMap
+{
+    "point-list": "point-list",
+    "line-list": "line-list",
+    "line-strip": "line-strip",
+    "triangle-list": "triangle-list",
+    "triangle-strip": "triangle-strip",
 }
