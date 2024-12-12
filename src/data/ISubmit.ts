1e4d9dfafab46819ae078ee7005ed3a32cf89a98
feng <908087098@qq.com>
2024-12-12 23:03:25 +0800

    ISubmit

--- src/data/ISubmit.ts
+++ src/data/ISubmit.ts
@@ -2,6 +2,8 @@ import { ICommandEncoder } from "./ICommandEncoder";
 
 /**
  * 一次 GPU 提交。
+ * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/GPUQueue/submit
  */
 export interface ISubmit
 {
