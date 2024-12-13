75f2de3976b484d882e250d694b835199cb948fd
feng <908087098@qq.com>
2024-12-13 16:12:27 +0800

    IColorTargetState.writeMask

--- src/data/IRenderPipeline.ts
+++ src/data/IRenderPipeline.ts
@@ -63,13 +63,15 @@ export interface IColorTargetState
     readonly blend?: IBlendState;
 
     /**
-     * 控制那些颜色分量是否可以被写入到帧缓冲器。
+     * 控制那些颜色分量是否可以被写入到颜色中。
      *
      * [red: boolean, green: boolean, blue: boolean, alpha: boolean]
      *
+     * 默认 [true,true,true,true]
+     * 
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/colorMask
      */
-    // readonly writeMask?: IWriteMask;
+    readonly writeMask?: IWriteMask;
 }
 
 export type IWriteMask = [red: boolean, green: boolean, blue: boolean, alpha: boolean];
