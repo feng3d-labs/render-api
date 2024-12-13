8a07f0e4cd278b91515d7364da489d24f0422cba
feng <908087098@qq.com>
2024-12-13 17:22:21 +0800

    IBlendComponent

--- src/data/IRenderPipeline.ts
+++ src/data/IRenderPipeline.ts
@@ -50,7 +50,9 @@ export interface IFragmentState
      * A list of {@link GPUColorTargetState} defining the formats and behaviors of the color targets
      * this pipeline writes to.
      * 
+     * 定义了该管道写入的颜色目标的格式和行为。
      * 
+     * 注：WebGL中没法分别对每个颜色附件进行设置，统一使用第一项（targets[0]）设置！
      */
     readonly targets?: readonly IColorTargetState[];
 }
@@ -58,7 +60,12 @@ export interface IFragmentState
 export interface IColorTargetState
 {
     /**
-     * 混合状态。
+     * The blending behavior for this color target. If left undefined, disables blending for this
+     * color target.
+     * 
+     * 定义如何混合到目标颜色中。
+     * 
+     * 默认 `undefined`，表示不进行混合。
      */
     readonly blend?: IBlendState;
 
@@ -78,6 +85,12 @@ export type IWriteMask = [red: boolean, green: boolean, blue: boolean, alpha: bo
 
 /**
  * 混合状态。
+ * 
+ * 定义如何混合到目标颜色中。
+ *
+ * ### WebGPU
+ * 
+ * @see https://gpuweb.github.io/gpuweb/#dictdef-gpublendstate
  */
 export interface IBlendState
 {
@@ -94,29 +107,68 @@ export interface IBlendState
 
 /**
  * 为颜色或alpha通道定义相应渲染目标的混合行为。
+ * 
+ * ### WebGL
+ * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendEquation
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
+ * 
+ * ### WebGPU
+ * 
+ * @see https://gpuweb.github.io/gpuweb/#dictdef-gpucolortargetstate
  */
 export interface IBlendComponent
 {
-    // /**
-    //  * 混合方式，默认 FUNC_ADD，源 + 目标。
-    //  *
-    //  * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendEquation
-    //  */
-    // operation?: GLBlendEquation;
+    /**
+     * 混合方式。
+     * 
+     * 默认为 "add"。
+     * 
+     * 当 `operation` 值为 "min" 或 "max" 时， `srcFactor` 与 `dstFactor` 将会被引擎自动使用 "one"。
+     * 
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendEquation
+     */
+    readonly operation?: IBlendOperation;
 
-    // /**
-    //  * 源混合因子，默认 SRC_ALPHA，将所有颜色乘以源alpha值。
-    //  *
-    //  * @see GLBlendFactor
-    //  * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
-    //  */
-    // srcFactor?: GLBlendFactor;
+    /**
+     * 源混合因子。
+     * 
+     * 默认为 "one"。
+     * 
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
+     */
+    readonly srcFactor?: IBlendFactor;
 
-    // /**
-    //  * 目标混合因子，默认 ONE_MINUS_SRC_ALPHA，将所有颜色乘以1减去源alpha值。
-    //  *
-    //  * @see GLBlendFactor
-    //  * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
-    //  */
-    // dstFactor?: GLBlendFactor;
+    /**
+     * 目标混合因子。
+     * 
+     * 默认为 "zero"。
+     * 
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
+     */
+    readonly dstFactor?: IBlendFactor;
+}
+
+export type IBlendOperation = "add" | "subtract" | "reverse-subtract" | "min" | "max";
+
+/**
+ * @see https://gpuweb.github.io/gpuweb/#enumdef-gpublendfactor
+ */
+export type IBlendFactor = IBlendFactorMap[keyof IBlendFactorMap];
+
+export interface IBlendFactorMap
+{
+    "zero": "zero";
+    "one": "one";
+    "src": "src";
+    "one-minus-src": "one-minus-src";
+    "src-alpha": "src-alpha";
+    "one-minus-src-alpha": "one-minus-src-alpha";
+    "dst": "dst";
+    "one-minus-dst": "one-minus-dst";
+    "dst-alpha": "dst-alpha";
+    "one-minus-dst-alpha": "one-minus-dst-alpha";
+    "src-alpha-saturated": "src-alpha-saturated";
+    "constant": "constant";
+    "one-minus-constant": "one-minus-constant";
 }
