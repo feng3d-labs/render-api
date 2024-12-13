429425dc3371c2018007f5202ddc497ac60bbc2e
feng <908087098@qq.com>
2024-12-13 12:46:32 +0800

    IRenderPipeline.vertex

--- src/data/IRenderPipeline.ts
+++ src/data/IRenderPipeline.ts
@@ -11,4 +11,20 @@ export interface IRenderPipeline
      * 图元拓扑结构。
      */
     readonly primitive?: IPrimitiveState;
+
+    /**
+     * 描述顶点着色器源码入口点以及顶点属性缓冲区布局。
+     */
+    readonly vertex: IVertexState;
+}
+
+/**
+ * 顶点程序阶段。
+ */
+export interface IVertexState
+{
+    /**
+     * 着色器代码。
+     */
+    readonly code: string;
 }
