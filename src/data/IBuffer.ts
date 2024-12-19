8a722068faca0e636330a2d3175204d6e8cb4c9e
feng <908087098@qq.com>
2024-12-19 20:34:35 +0800

    IWriteBuffer

--- src/data/IBuffer.ts
+++ src/data/IBuffer.ts
@@ -22,4 +22,33 @@ export interface IBuffer
      * 缓冲区数据。
      */
     data?: TypedArray;
+
+    /**
+     * 写缓冲区。
+     */
+    writeBuffers?: IWriteBuffer[];
+}
+
+export interface IWriteBuffer
+{
+    bufferOffset?: number;
+
+    /**
+     * 写入缓冲区数据。
+     */
+    data: ArrayBufferLike | TypedArray;
+
+    /**
+     * 数据偏移。
+     * 
+     * 默认为 0 。
+     * 
+     * 当写入的数据类型为 {@link ArrayBufferLike} 时单位为字节，当数据类型为 {@link TypedArray} 时单位为元素。
+     */
+    dataOffset?: number;
+
+    /**
+     * 写入数据尺寸。
+     */
+    size?: number;
 }
