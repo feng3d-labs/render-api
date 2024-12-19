e0f3de1b230e0afa601cb0b741a4c9148e445971
feng <908087098@qq.com>
2024-12-19 21:30:49 +0800

    IWriteBuffer

--- src/data/IBuffer.ts
+++ src/data/IBuffer.ts
@@ -25,12 +25,17 @@ export interface IBuffer
 
     /**
      * 写缓冲区。
+     * 
+     * {@link GPUQueue.writeBuffer}
      */
     writeBuffers?: IWriteBuffer[];
 }
 
 export interface IWriteBuffer
 {
+    /**
+     * GPU缓冲区写入起始位置。
+     */
     bufferOffset?: number;
 
     /**
@@ -39,7 +44,7 @@ export interface IWriteBuffer
     data: ArrayBufferLike | TypedArray;
 
     /**
-     * 数据偏移。
+     * 读取数据的起始位置。
      * 
      * 默认为 0 。
      * 
@@ -49,6 +54,8 @@ export interface IWriteBuffer
 
     /**
      * 写入数据尺寸。
+     * 
+     * 当写入的数据类型为 {@link ArrayBufferLike} 时单位为字节，当数据类型为 {@link TypedArray} 时单位为元素。
      */
     size?: number;
 }
