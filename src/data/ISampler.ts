eb56a083d1991fa7aab3e6548435776770fa5223
feng <908087098@qq.com>
2025-01-02 16:55:59 +0800

    ISampler.addressModeU addressModeV addressModeW 默认为 "repeat"

--- src/data/ISampler.ts
+++ src/data/ISampler.ts
@@ -6,7 +6,7 @@ export interface ISampler
     /**
      * 用于指定纹理在水平方向（即S或U坐标轴）上的寻址模式。
      * 
-     * 默认 "clamp-to-edge"。
+     * 默认 "repeat"。
      * 
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_wrap_s
      * @see https://gpuweb.github.io/gpuweb/#dom-gpusamplerdescriptor-addressmodeu
@@ -16,7 +16,7 @@ export interface ISampler
     /**
      * 用于指定纹理在垂直方向（即T或V坐标轴）上的寻址模式。
      * 
-     * 默认 "clamp-to-edge"。
+     * 默认 "repeat"。
      * 
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_wrap_t
      * @see https://gpuweb.github.io/gpuweb/#dom-gpusamplerdescriptor-addressmodev
@@ -26,7 +26,7 @@ export interface ISampler
     /**
      * 用于指定纹理在深度方向（即R或W坐标轴）上的寻址模式。用于3D纹理或者纹理数组。
      * 
-     * 默认 "clamp-to-edge"。
+     * 默认 "repeat"。
      * 
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_wrap_r
      * @see https://gpuweb.github.io/gpuweb/#dom-gpusamplerdescriptor-addressmodew
