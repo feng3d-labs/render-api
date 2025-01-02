1e724ef22043835f8b549b7ac56831f06d0aee60
feng <908087098@qq.com>
2025-01-02 13:28:36 +0800

    ISampler.magFilter minFilter mipmapFilter

--- src/data/ISampler.ts
+++ src/data/ISampler.ts
@@ -32,9 +32,51 @@ export interface ISampler
      * @see https://gpuweb.github.io/gpuweb/#dom-gpusamplerdescriptor-addressmodew
      */
     addressModeW?: IAddressMode;
+
+    /**
+     * 指定样本足迹小于或等于一个纹素时的采样行为
+     * 
+     * 默认 "nearest"。
+     * 
+     * @see https://www.orillusion.com/zh/webgpu.html#dom-gpusamplerdescriptor-magfilter
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_mag_filter
+     */
+    magFilter?: IFilterMode;
+
+    /**
+     * 指定样本足迹大于一个纹素时的采样行为。
+     * 
+     * 默认 "nearest"。
+     * 
+     * 注：WebGL中如果使用mipmap相关值时需要设置 {@link ISampler.mipmapFilter} 值。
+     * 
+     * @see https://www.orillusion.com/zh/webgpu.html#dom-gpusamplerdescriptor-minfilter
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_min_filter
+     */
+    minFilter?: IFilterMode;
+
+    /**
+     * 指定在 mipmap 级别之间进行采样的行为。
+     * 
+     * 默认 "nearest"。
+     * 
+     * @see https://www.orillusion.com/zh/webgpu.html#dom-gpusamplerdescriptor-mipmapfilter
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGL2RenderingContext/samplerParameter#gl.texture_min_filter
+     */
+    mipmapFilter?: IMipmapFilterMode;
 }
 
 /**
  * 纹理坐标寻址模式。
  */
 export type IAddressMode = "clamp-to-edge" | "repeat" | "mirror-repeat";
+
+/**
+ * 描述采样器在采样足迹与一个纹素不完全匹配时的行为。
+ */
+export type IFilterMode = "nearest" | "linear";
+
+/**
+ * 描述采样器在采样足迹与mipmap层级不完全匹配时的行为。
+ */
+export type IMipmapFilterMode = "nearest" | "linear";
