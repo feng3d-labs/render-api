29b7f28a69e7469c02ebe3dc14af59be5680e295
feng <908087098@qq.com>
2025-03-12 20:25:31 +0800

    refactor(data): 优化 CanvasTexture 接口定义
    - 移除了 CanvasTexture 接口中的冗余注释
    - 添加了对 context 属性的注释说明
    - 简化了接口的描述，使其更加通用

--- src/data/CanvasTexture.ts
+++ src/data/CanvasTexture.ts
@@ -1,13 +1,14 @@
 import { CanvasContext } from "./CanvasContext";
 
 /**
- * 画布纹理，从画布的WebGPU上下文获取纹理
- *
- * 注：只在WebGPU上支持。
+ * 画布纹理，从画布的上下文获取纹理
  */
 export interface CanvasTexture
 {
     __type__?: "CanvasTexture";
 
+    /**
+     * 画布上下文。
+     */
     context: CanvasContext;
 }
