2008d80f9073aa8bd298bd5a3cde368e6b1f99cb
feng <908087098@qq.com>
2025-07-04 08:10:44 +0800

    refactor(src): 更新 BlendComponent 类型定义
    - 将 IBlendOperation 和 IBlendFactor 类型重命名为 BlendOperation 和 BlendFactor，以提高一致性和可读性。

--- src/data/BlendComponent.ts
+++ src/data/BlendComponent.ts
@@ -21,7 +21,7 @@ export interface BlendComponent
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendEquation
      */
-    readonly operation?: IBlendOperation;
+    readonly operation?: BlendOperation;
 
     /**
      * 源混合因子。
@@ -30,7 +30,7 @@ export interface BlendComponent
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
      */
-    readonly srcFactor?: IBlendFactor;
+    readonly srcFactor?: BlendFactor;
 
     /**
      * 目标混合因子。
@@ -39,15 +39,15 @@ export interface BlendComponent
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendFunc
      */
-    readonly dstFactor?: IBlendFactor;
+    readonly dstFactor?: BlendFactor;
 }
 
-export type IBlendOperation = 'add' | 'subtract' | 'reverse-subtract' | 'min' | 'max';
+export type BlendOperation = 'add' | 'subtract' | 'reverse-subtract' | 'min' | 'max';
 
 /**
  * @see https://gpuweb.github.io/gpuweb/#enumdef-gpublendfactor
  */
-export type IBlendFactor = IBlendFactorMap[keyof IBlendFactorMap];
+export type BlendFactor = IBlendFactorMap[keyof IBlendFactorMap];
 
 export interface IBlendFactorMap
 {
