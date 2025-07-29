fcb9a42dfb25bde1bff5c722909464519bb6efc3
feng <908087098@qq.com>
2025-07-29 23:44:26 +0800

    refactor(src): 更新 CanvasContext 和 CanvasTexture 接口定义
    - 在 CanvasContext 中添加可选的宽度和高度属性。
    - 从 CanvasTexture 中移除不再使用的 _canvasSizeVersion 属性。

--- src/data/CanvasContext.ts
+++ src/data/CanvasContext.ts
@@ -9,4 +9,14 @@ export interface CanvasContext
      * 可能是画布的编号，也可能是画布元素或者离屏画布。
      */
     readonly canvasId: string | HTMLCanvasElement | OffscreenCanvas;
+
+    /**
+     * 画布宽度。
+     */
+    readonly width?: number;
+
+    /**
+     * 画布高度。
+     */
+    readonly height?: number;
 }
--- src/data/CanvasTexture.ts
+++ src/data/CanvasTexture.ts
@@ -9,13 +9,4 @@ export interface CanvasTexture
      * 画布上下文。
      */
     context: CanvasContext;
-
-    /**
-     * 画布尺寸发生变化后引擎自动递增。
-     *
-     * 引擎内部监听这个值，在画布尺寸发生变化后重新获取纹理。
-     *
-     * @private
-     */
-    _canvasSizeVersion?: number;
 }
