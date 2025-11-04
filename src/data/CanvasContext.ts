ae3e64feec3a93a6b9b44090e33e81ec63d011a1
feng <908087098@qq.com>
2025-11-04 20:08:50 +0800

    refactor(src): 移除 CanvasContext 接口中的宽高属性
    - 从 CanvasContext 接口中移除可选的 width 和 height 属性，以简化接口结构。
    - 该变更旨在提高代码的清晰度和可维护性。

--- src/data/CanvasContext.ts
+++ src/data/CanvasContext.ts
@@ -9,14 +9,4 @@ export interface CanvasContext
      * 可能是画布的编号，也可能是画布元素或者离屏画布。
      */
     readonly canvasId: string | HTMLCanvasElement | OffscreenCanvas;
-
-    /**
-     * 画布宽度。
-     */
-    readonly width?: number;
-
-    /**
-     * 画布高度。
-     */
-    readonly height?: number;
 }
